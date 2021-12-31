7993c6aef39468da5ad1b323e4bf368306324232
jardelgoncalves <jardelgoncalves1996@gmail.com>
2021-12-31 16:44:29 -0300

    feat: 🎸 add theme object

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+import { colors } from './tokens/colors';
+import { layers } from './tokens/layers';
+import { typography } from './tokens/typography';
+import { breakpoints } from './tokens/breakpoints';
+
+export const theme = {
+  colors,
+  layers,
+  typography,
+  breakpoints,
+};
