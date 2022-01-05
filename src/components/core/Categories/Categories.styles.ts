646e116205c15b4bc7e7bba3280e765ab7d7b672
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-01-04 23:39:25 -0300

    feat: 🎸 add style to categories component

---
+++ src/components/core/Categories/Categories.styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 8px;
+`;
+
+type ItemProps = {
+  isActive?: boolean;
+};
+
+export const Item = styled.button<ItemProps>`
+  height: 54px;
+  width: 72px;
+  border-radius: 8px;
+  transition: filter 0.2s;
+  filter: ${({ isActive }) => (isActive ? 'saturate(100%)' : 'saturate(0%)')};
+
+  img {
+    height: 54px;
+    width: 72px;
+    border-radius: 8px;
+    object-fit: cover;
+  }
+
+  &:focus {
+    box-shadow: none;
+  }
+`;
