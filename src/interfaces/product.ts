96110d8abe1a00fa3cb6565e38de1b2611e41a35
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-01-11 23:02:11 -0300

    feat: 🎸 add new props

--- src/interfaces/product.ts
+++ src/interfaces/product.ts
@@ -2,6 +2,8 @@ export type Product = {
   id: string;
   thumb: string;
   name: string;
+  category: 'pizza' | 'sandwich' | 'drink' | 'ice-cream' | 'fries';
+  popular: boolean;
   ingredients?: string;
   rating: number;
   price: number;
