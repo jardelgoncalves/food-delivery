ab4ab792f10124c7b6d815c221ddf7720a256690
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-01-12 22:17:24 -0300

    refactor: 💡 remove custom hook to only call api

--- src/useCases/products.ts
+++ src/useCases/products.ts
@@ -1,28 +1,6 @@
-import type { Product } from 'interfaces/product';
-import { useEffect, useState } from 'react';
 import api from 'services/api';
 
-export function usePopularProductsApi() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    try {
-      setLoading(true);
-      api.get('/products-popular').then(({ data }) => {
-        setProducts(data || []);
-      });
-    } catch (err) {
-      setError('an unexpected error occurred');
-    } finally {
-      setLoading(false);
-    }
-  }, []);
-
-  return {
-    products,
-    loading,
-    error,
-  };
+export async function getPopularProducts() {
+  const { data } = await api.get('/products-popular');
+  return data;
 }
