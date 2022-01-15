894ec4696f4a8c22ce57c0a7313700f37002d157
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-01-15 20:20:16 -0300

    feat: 🎸 add intro section to about us page

---
+++ src/containers/AboutUs/Intro/index.tsx
@@ -0,0 +1,34 @@
+import * as Styles from './Intro.styles';
+
+export function Intro() {
+  return (
+    <Styles.Container>
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In luctus diam
+        urna, vitae vestibulum diam congue sed. In finibus mauris orci, non
+        sollicitudin massa congue sed. Nunc semper lacus augue, feugiat faucibus
+        sapien rutrum pulvinar. Nullam vulputate felis ac ullamcorper viverra.
+        Maecenas nisl urna, malesuada quis sagittis dapibus, rhoncus sed lacus.
+        Pellentesque gravida et sem nec auctor. Sed elementum auctor neque, et
+        finibus nisi rhoncus id. Proin imperdiet placerat lorem a pellentesque.
+        Etiam mollis elit at pulvinar convallis. Mauris vitae eros rutrum,
+        sagittis metus quis, vehicula tellus. Vivamus faucibus, libero vitae
+        fermentum egestas, lorem eros volutpat felis, sed rhoncus lorem massa et
+        mauris.
+      </p>
+      <p>
+        Vestibulum nulla sem, ultricies non dictum ac, varius sit amet felis.
+        Duis ut ex malesuada, finibus ante sit amet, molestie libero. Aenean
+        congue dolor in mi convallis feugiat. Nulla facilisi. Duis ipsum est,
+        tempor quis mauris sit amet, vulputate elementum sapien. Nulla luctus
+        purus sed mauris hendrerit venenatis. Integer a ultrices dui, ac
+        sagittis nibh. Nunc vestibulum egestas diam et ullamcorper. In et leo
+        lorem. Morbi volutpat ac purus vel mollis. Aliquam semper lectus magna,
+        at tincidunt augue imperdiet at. Suspendisse a sem eget nunc vulputate
+        elementum nec vitae lectus. Nulla facilisi. Morbi sed diam a urna
+        imperdiet pharetra quis eu metus.
+      </p>
+    </Styles.Container>
+  );
+}
