d1ede55f2a3eb6a51d5ce91d717b7a6ca700f640
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-01-15 17:07:48 -0300

    feat: 🎸 add cart slice in root reducer

--- src/store.ts
+++ src/store.ts
@@ -2,10 +2,12 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
 import popularProductSlice from 'features/product/popular-product-slice';
 import productSlice from 'features/product/products-slice';
+import cartSlice from 'features/cart/cart-slice';
 
 const rootReducer = combineReducers({
   popularProduct: popularProductSlice,
   product: productSlice,
+  cart: cartSlice,
 });
 
 const store = configureStore({
