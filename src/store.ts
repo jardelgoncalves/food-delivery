29ac046ae118e614c451dd80d15fd37816619917
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-01-15 00:01:26 -0300

    feat: 🎸 add product to reducer

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
 import popularProductSlice from 'features/product/popular-product-slice';
+import productSlice from 'features/product/products-slice';
 
 const rootReducer = combineReducers({
   popularProduct: popularProductSlice,
+  product: productSlice,
 });
 
 const store = configureStore({
