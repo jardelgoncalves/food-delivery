77922027d9a5a9abaead881e1605635cb2db8318
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-01-15 17:08:54 -0300

    feat: 🎸 add notification item with quantity product

--- src/components/core/Header/index.tsx
+++ src/components/core/Header/index.tsx
@@ -1,12 +1,17 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
 
 import { BsHandbag } from 'react-icons/bs';
 import Logo from 'assets/logo.png';
 
+import { handleModal } from 'features/cart/cart-slice';
+import { RootState } from 'store';
 import * as Styles from './Header.styles';
 
 export function Header() {
+  const { products } = useSelector((state: RootState) => state.cart);
   const { pathname } = useLocation();
+  const dispatch = useDispatch();
 
   return (
     <Styles.Container>
@@ -42,8 +47,14 @@ export function Header() {
             </Link>
           </li>
         </ul>
-        <Styles.BagButton hasItem aria-label="Bag">
+        <Styles.BagButton
+          aria-label="Open Cart"
+          onClick={() => dispatch(handleModal(true))}
+        >
           <BsHandbag size={22} />
+          {!!products.length && (
+            <Styles.NotificationItem>{products.length}</Styles.NotificationItem>
+          )}
         </Styles.BagButton>
       </nav>
     </Styles.Container>
