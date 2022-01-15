ec0781d541437b943f0ff5df1ed746c1a8f9deaf
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-01-15 20:20:36 -0300

    feat: 🎸 add about us container

---
+++ src/containers/AboutUs/index.tsx
@@ -0,0 +1,15 @@
+import { Layout } from 'components/core/Layout';
+import { Gallery } from './Gallery';
+
+import { Intro } from './Intro';
+
+function AboutUs() {
+  return (
+    <Layout>
+      <Intro />
+      <Gallery />
+    </Layout>
+  );
+}
+
+export default AboutUs;
